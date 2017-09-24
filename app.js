d06982e6843bee9f11592770f642bd85de9af89e
Lavish Thakkar <lavishthakkar@hotmail.com>
2017-09-24 19:02:07 +0530

    Install Dependencies Configured Mongoose

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var dbconfig = require('./config/database');
+mongoose.Promise = require('bluebird');
+mongoose.connect(dbconfig.url);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url': 'mongodb://lavisht22:simplepassword123@ds147974.mlab.com:47974/borrowlend'
+};
